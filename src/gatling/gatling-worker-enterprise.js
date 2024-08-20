39b344ca07d0ca4271015def7cf21477a268df6b
Florent Huck <flovntp@gmail.com>
2024-08-20 11:50:26 +0200

    adding Activity Script gatling-worker-enterprise.js (change message)

--- src/gatling/gatling-worker-enterprise.js
+++ src/gatling/gatling-worker-enterprise.js
@@ -5,11 +5,5 @@ if (!variables.gatling_enterprise_api_token) {
     console.log("Please define an environment variable with your GATLING_ENTERPRISE_API_TOKEN using command: ");
     console.log("upsun project:curl /integrations/<INTEGRATION_ID>/variables -X POST -d '{\"name\": \"gatling_enterprise_api_token\", \"value\": \"<GATLING_ENTERPRISE_API_TOKEN>\", \"is_sensitive\": true, \"is_json\": false}'");
 } else {
-    npx gatling enterprise-start --enterprise-simulation=simulation;
-
-    if (!resp.ok) {
-        console.log("Failed to add a marker \"" + '(' + activity.state + ') ' + activity.text + "\" on your Blackfire environment, status code was " + resp.status);
-    } else {
-        console.log("Marker \"" + '(' + activity.state + ') ' + activity.text + "\" added on Blackfire");
-    }
+    npx gatling enterprise-start --enterprise-simulation=simulation --wait-for-run-end --non-interactive;
 }
