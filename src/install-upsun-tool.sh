52509ba5ff3d57dfbbcf5ff89131f73b84c353cc
Florent Huck <flovntp@gmail.com>
2024-09-02 14:44:48 +0200

    install-upsun-tool.sh: changing UPSUN_PROJECT to UPSUN_TOOL

--- src/install-upsun-tool.sh
+++ src/install-upsun-tool.sh
@@ -1,29 +1,27 @@
-# install-scalsun.sh
+# curl -fsS https://raw.githubusercontent.com/upsun/snippets/main/src/install-upsun-tool.sh | bash /dev/stdin "scalsun" 
 
 # contributors:
 #  - Florent HUCK <florent.huck@platform.sh>
-
-ASSET_ID='';
 
 run() {
    # Run the compilation process.
    cd $PLATFORM_CACHE_DIR || exit 1;
 
-   UPSUN_PROJECT=$1;
+   UPSUN_TOOL=$1;
    UPSUN_VERSION=$2;
 
-   UPSUN_BINARY="${UPSUN_PROJECT}"
+   UPSUN_BINARY="${UPSUN_TOOL}"
 
    rm -Rf ${PLATFORM_CACHE_DIR}/${UPSUN_BINARY}
 
    if [ ! -f "${PLATFORM_CACHE_DIR}/${UPSUN_BINARY}" ]; then
-       ensure_source "$UPSUN_PROJECT" "$UPSUN_VERSION";
-       download_binary "$UPSUN_PROJECT" "$UPSUN_VERSION";
-       move_binary "$UPSUN_PROJECT" "$UPSUN_BINARY";
+       ensure_source "$UPSUN_TOOL" "$UPSUN_VERSION";
+       download_binary "$UPSUN_TOOL" "$UPSUN_VERSION";
+       move_binary "$UPSUN_TOOL" "$UPSUN_BINARY";
    fi
 
-   copy_lib "$UPSUN_PROJECT" "$UPSUN_BINARY";
-   echo "$UPSUN_PROJECT installation successful"
+   copy_lib "$UPSUN_TOOL" "$UPSUN_BINARY";
+   echo "$UPSUN_TOOL installation successful"
 }
 
 copy_lib() {
@@ -31,42 +29,39 @@ copy_lib() {
    echo " Copying compiled extension to PLATFORM_APP_DIR "
    echo "------------------------------------------------"
 
-   UPSUN_PROJECT=$1;
+   UPSUN_TOOL=$1;
    UPSUN_BINARY=$2;
 
-   cp "${PLATFORM_CACHE_DIR}/${UPSUN_BINARY}/${UPSUN_PROJECT}" "${PLATFORM_APP_DIR}/${UPSUN_PROJECT}";
+   cp "${PLATFORM_CACHE_DIR}/${UPSUN_BINARY}/${UPSUN_TOOL}" "${PLATFORM_APP_DIR}/${UPSUN_TOOL}";
    cd ${PLATFORM_APP_DIR};
-   chmod +x ${UPSUN_PROJECT};
+   chmod +x ${UPSUN_TOOL};
    echo "Success"
 }
 
 ensure_source() {
    echo "-----------------------------------------------------------------------------------"
-   echo " Ensuring that the $UPSUN_PROJECT binary folder is available and up to date "
+   echo " Ensuring that the $UPSUN_TOOL binary folder is available and up to date "
    echo "-----------------------------------------------------------------------------------"
 
-   UPSUN_PROJECT=$1;
+   UPSUN_TOOL=$1;
    UPSUN_VERSION=$2;
 
-   mkdir -p "$PLATFORM_CACHE_DIR/$UPSUN_PROJECT/$UPSUN_VERSION";
+   mkdir -p "$PLATFORM_CACHE_DIR/$UPSUN_TOOL/$UPSUN_VERSION";
    
-   cd "$PLATFORM_CACHE_DIR/$UPSUN_PROJECT/$UPSUN_VERSION" || exit 1;
+   cd "$PLATFORM_CACHE_DIR/$UPSUN_TOOL/$UPSUN_VERSION" || exit 1;
    echo "Success"
 }
 
 download_binary() {
    echo "---------------------------------------------------------------------"
-   echo " Downloading $UPSUN_PROJECT binary source code "
+   echo " Downloading $UPSUN_TOOL binary source code "
    echo "---------------------------------------------------------------------"
-   UPSUN_PROJECT=$1;
+   UPSUN_TOOL=$1;
    UPSUN_VERSION=$2;
    
-   BINARY_NAME="$UPSUN_PROJECT-$UPSUN_VERSION-linux-amd64.tar.gz"
+   BINARY_NAME="$UPSUN_TOOL-$UPSUN_VERSION-linux-amd64.tar.gz"
    
    get_asset_id
-   
-  ls -la 
-   echo "assetId $ASSET_ID"
    
    curl -L \
      -H "Accept: application/octet-stream" \
@@ -74,30 +69,24 @@ download_binary() {
      "https://api.github.com/repos/upsun/$TOOL/releases/assets/$ASSET_ID" \
      -o $BINARY_NAME 
    tar -xvf $BINARY_NAME
-  pwd 
-  ls -la 
+
    echo "Success" 
 }
 
-get_asset_id() {
-  
+get_asset_id() {  
    ASSET_ID=$(curl --silent -L \
     -H "Accept: application/vnd.github+json" \
-    -H "Authorization: Bearer $GITHUB_API_TOKEN" "https://api.github.com/repos/upsun/$UPSUN_PROJECT/releases" \
+    -H "Authorization: Bearer $GITHUB_API_TOKEN" "https://api.github.com/repos/upsun/$UPSUN_TOOL/releases" \
     | jq -r --arg VERSION "$VERSION" --arg BINARY_NAME "$BINARY_NAME" 'map(select(.name==$VERSION)) | .[0].assets | map(select(.name==$BINARY_NAME)) | .[].id')
-    
-    echo "Authorization: Bearer $GITHUB_API_TOKEN" "https://api.github.com/repos/upsun/$UPSUN_PROJECT/releases"
-    
-    echo "assetID = $ASSET_ID";
 }
 
 move_binary() {
    echo "-----------------------------------------------------"
-   echo " Moving and caching ${UPSUN_PROJECT} binary "
+   echo " Moving and caching ${UPSUN_TOOL} binary "
    echo "-----------------------------------------------------"
-   UPSUN_PROJECT=$1;
+   UPSUN_TOOL=$1;
    UPSUN_BINARY=$2;
-   cp -r "${PLATFORM_CACHE_DIR}/${UPSUN_PROJECT}/${UPSUN_VERSION}/${UPSUN_PROJECT}" "${PLATFORM_CACHE_DIR}/${UPSUN_BINARY}";
+   cp -r "${PLATFORM_CACHE_DIR}/${UPSUN_TOOL}/${UPSUN_VERSION}/${UPSUN_TOOL}" "${PLATFORM_CACHE_DIR}/${UPSUN_BINARY}";
    chmod +x "${PLATFORM_CACHE_DIR}/${UPSUN_BINARY}";
    echo "Success"
 }
@@ -105,13 +94,13 @@ move_binary() {
 ensure_environment() {
    # If not running in an Upsun/Platform.sh build environment, do nothing.
    if [ -z "${PLATFORM_CACHE_DIR}" ]; then
-       echo "Not running in an Upsun/Platform.sh build environment. Aborting $UPSUN_PROJECT installation.";
+       echo "Not running in an Upsun/Platform.sh build environment. Aborting $UPSUN_TOOL installation.";
        exit 0;
    fi
 }
 
 if [ -z "$1" ]; then
-  echo "Please define parameter for the tool you want to install: scalsun, provsun, ... ";
+  echo "Please define parameter for the tool you want to install: scalsun, ... ";
   echo "See https://github.com/upsun --> '***sun' repos ";
 else 
   TOOL=$1;
@@ -127,5 +116,4 @@ VERSION=$(curl --silent -H "Authorization: token $GITHUB_API_TOKEN" \
   -H 'Accept: application/vnd.github.v3.raw' \
   -L https://api.github.com/repos/upsun/$TOOL/tags | jq -r '.[0].name');
   
-
 run "$TOOL" "$VERSION"
