c001d5c34abf3ad0b431d2e2dedc3d6ae8eeb618
Paul Gilzow <paul@gilzow.com>
2025-01-14 15:35:51 -0600

    Update wp-config.php
    correct environment names to match what is generated by upsunify

--- examples/wordpress-vanilla/wordpress/wp-config.php
+++ examples/wordpress-vanilla/wordpress/wp-config.php
@@ -6,61 +6,61 @@ $site_host = 'localhost';
 
 // Update scheme and hostname for the requested page.
 if (isset($_SERVER['HTTP_HOST'])) {
-  $site_host = $_SERVER['HTTP_HOST'];
-  $site_scheme = !empty($_SERVER['HTTPS']) ? 'https' : 'http';
+    $site_host = $_SERVER['HTTP_HOST'];
+    $site_scheme = !empty($_SERVER['HTTPS']) ? 'https' : 'http';
 }
 
 if (getenv('PLATFORM_RELATIONSHIPS')) {
-	if(getenv('DB_NAME')){
+    if(getenv('DB_HOST')){
 
-		// Avoid PHP notices on CLI requests.
-		if (php_sapi_name() === 'cli') {
-			session_save_path("/tmp");
-		}
+        // Avoid PHP notices on CLI requests.
+        if (php_sapi_name() === 'cli') {
+            session_save_path("/tmp");
+        }
 
-		// We are using the first relationship called "database" found in your
-		// relationships. Note that you can call this relationship as you wish
-		// in your `.platform.app.yaml` file, but 'database' is a good name.
-		define( 'DB_NAME', getenv('DB_NAME'));
-		define( 'DB_USER', getenv('DB_USER'));
-		define( 'DB_PASSWORD', getenv('DB_PASSWORD'));
-		define( 'DB_HOST', getenv('DB_HOST'));
-		define( 'DB_CHARSET', 'utf8' );
-		define( 'DB_COLLATE', '' );
+        // We are using the first relationship called "database" found in your
+        // relationships. Note that you can call this relationship as you wish
+        // in your `.platform.app.yaml` file, but 'database' is a good name.
+        define( 'DB_NAME', getenv('DB_DATABASE'));
+        define( 'DB_USER', getenv('DB_USERNAME'));
+        define( 'DB_PASSWORD', getenv('DB_PASSWORD'));
+        define( 'DB_HOST', getenv('DB_HOST'));
+        define( 'DB_CHARSET', 'utf8' );
+        define( 'DB_COLLATE', '' );
 
-		// Debug mode should be disabled on Platform.sh. Set this constant to true
-		// in a wp-config-local.php file to skip this setting on local development.
-		if (!defined( 'WP_DEBUG' )) {
-			define( 'WP_DEBUG', false );
-		}
+        // Debug mode should be disabled on Platform.sh. Set this constant to true
+        // in a wp-config-local.php file to skip this setting on local development.
+        if (!defined( 'WP_DEBUG' )) {
+            define( 'WP_DEBUG', false );
+        }
 
-		// Set all of the necessary keys to unique values, based on the Platform.sh
-		// entropy value.
-		if (getenv('PLATFORM_PROJECT_ENTROPY')) {
-			$keys = [
-				'AUTH_KEY',
-				'SECURE_AUTH_KEY',
-				'LOGGED_IN_KEY',
-				'NONCE_KEY',
-				'AUTH_SALT',
-				'SECURE_AUTH_SALT',
-				'LOGGED_IN_SALT',
-				'NONCE_SALT',
-			];
-			$entropy = getenv('PLATFORM_PROJECT_ENTROPY');
-			foreach ($keys as $key) {
-				if (!defined($key)) {
-					define( $key, $entropy . $key );
-				}
-			}
-		}
-	}
+        // Set all of the necessary keys to unique values, based on the Platform.sh
+        // entropy value.
+        if (getenv('PLATFORM_PROJECT_ENTROPY')) {
+            $keys = [
+                'AUTH_KEY',
+                'SECURE_AUTH_KEY',
+                'LOGGED_IN_KEY',
+                'NONCE_KEY',
+                'AUTH_SALT',
+                'SECURE_AUTH_SALT',
+                'LOGGED_IN_SALT',
+                'NONCE_SALT',
+            ];
+            $entropy = getenv('PLATFORM_PROJECT_ENTROPY');
+            foreach ($keys as $key) {
+                if (!defined($key)) {
+                    define( $key, $entropy . $key );
+                }
+            }
+        }
+    }
 }
 else {
-  // Local configuration file should be in project root.
-  if (file_exists(dirname(__FILE__, 2) . '/wp-config-local.php')) {
-    include(dirname(__FILE__, 2) . '/wp-config-local.php');
-  }
+    // Local configuration file should be in project root.
+    if (file_exists(dirname(__FILE__, 2) . '/wp-config-local.php')) {
+        include(dirname(__FILE__, 2) . '/wp-config-local.php');
+    }
 }
 
 // Do not put a slash "/" at the end.
@@ -82,7 +82,7 @@ $table_prefix  = 'wp_';
 
 /** Absolute path to the WordPress directory. */
 if ( ! defined( 'ABSPATH' ) ) {
-  define( 'ABSPATH', dirname( __FILE__ ) . '/' );
+    define( 'ABSPATH', dirname( __FILE__ ) . '/' );
 }
 
 /** Sets up WordPress vars and included files. */
