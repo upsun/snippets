33afec63d5845bd6524a06e837b3611e6e93201c
Florent Huck <flovntp@gmail.com>
2025-01-24 14:56:14 +0100

    generic script to install asset from any GitHub org/repo

---
+++ src/install-github-asset.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+# This generic script install a $GITHUB_ORG/$TOOL_NAME (corresponding asset) in the $PLATFORM_APP_DIR/bin folder of your app container
+# in the hooks.build of your Upsun/Platform.sh configuration: 
+# curl -fsS https://raw.githubusercontent.com/upsun/snippets/main/src/install-github-asset.sh | bash /dev/stdin "jgm/pandoc" [<version>] 
+# contributors:
+#  - Florent HUCK <florent.huck@platform.sh>
+
+run() {
+   set -e
+   # check if we are on an Upsun/Platform.sh 
+   ensure_environment
+   
+   # Get first parameter as the Github identifier: <org>/<repo>
+   if [ -z "$1" ]; then
+     echo "Please define 1st parameter for the Github organization and the repository where to find the tool: jgm/pandoc, ... ";
+   else   
+     GITHUB_ORG=$(echo "$1" | awk -F '/' '{print $1}');
+     TOOL_NAME=$(echo "$1" | awk -F '/' '{print $2}');
+   fi
+   
+   if [ -z "$2" ]; then
+     get_latest_version
+   else
+     echo "You define a specific version for $GITHUB_ORG/$TOOL_NAME: $2"
+     TOOL_VERSION=$2
+   fi
+
+   # Run the compilation process.
+   cd $PLATFORM_CACHE_DIR || exit 1;
+   
+   if [ ! -f "${PLATFORM_CACHE_DIR}/${TOOL_NAME}/${TOOL_VERSION}/${TOOL_NAME}-${TOOL_VERSION}/bin/${TOOL_NAME}" ]; then
+       ensure_source 
+       download_binary;
+       move_binary;
+   fi
+
+   copy_lib "$TOOL_NAME" "$TOOL_VERSION";
+   echo "$TOOL_NAME installation successful"
+   echo "use it using command: bin/$TOOL_NAME"
+}
+
+copy_lib() {
+   echo "--------------------------------------------------------------------------------------"
+   echo " Copying $TOOL_NAME asset from PLATFORM_CACHE_DIR to PLATFORM_APP_DIR "
+   echo "--------------------------------------------------------------------------------------"
+
+   mkdir -p ${PLATFORM_APP_DIR}/bin
+   cp -r "${PLATFORM_CACHE_DIR}/${TOOL_NAME}/${TOOL_NAME}" "${PLATFORM_APP_DIR}/bin";
+   cd ${PLATFORM_APP_DIR}/bin;
+   chmod +x "${TOOL_NAME}";
+   echo "Success"
+}
+
+ensure_source() {
+   echo "--------------------------------------------------------------------------------------"
+   echo " Ensuring that the $TOOL_NAME/$TOOL_VERSION binary folder is available and up to date "
+   echo "--------------------------------------------------------------------------------------"
+
+   mkdir -p "$PLATFORM_CACHE_DIR/$TOOL_NAME/$TOOL_VERSION";
+
+   cd "$PLATFORM_CACHE_DIR/$TOOL_NAME/$TOOL_VERSION" || exit 1;
+   echo "Success"
+}
+
+download_binary() {
+   echo "--------------------------------------------------------------------------------------"
+   echo " Downloading $TOOL_NAME binary source code "
+   echo "--------------------------------------------------------------------------------------"
+   
+   BINARY_NAME="$TOOL_NAME-$TOOL_VERSION-linux-amd64.tar.gz"
+   get_asset_id
+   
+   curl -L \
+     -H "Accept: application/octet-stream" "https://api.github.com/repos/$GITHUB_ORG/$TOOL_NAME/releases/assets/$ASSET_ID" \
+     -o $BINARY_NAME
+   tar -xvf $BINARY_NAME
+
+   echo "Success"
+}
+
+get_asset_id() {
+   ASSET_ID=$(curl --silent -L \
+    -H "Accept: application/vnd.github+json" "https://api.github.com/repos/$GITHUB_ORG/$TOOL_NAME/releases" \
+    | jq -r --arg BINARY_NAME "$BINARY_NAME" '.[0].assets | map(select(.name==$BINARY_NAME)) | .[].id')
+}
+
+move_binary() {
+   echo "--------------------------------------------------------------------------------------"
+   echo " Moving and caching ${TOOL_NAME} binary "
+   echo "--------------------------------------------------------------------------------------"
+   
+   # copy new version in cache
+   cp -r "${PLATFORM_CACHE_DIR}/${TOOL_NAME}/${TOOL_VERSION}/${TOOL_NAME}-${TOOL_VERSION}/bin/${TOOL_NAME}" "${PLATFORM_CACHE_DIR}/${TOOL_NAME}";
+   echo "Success"
+}
+
+ensure_environment() {
+   # If not running in an Upsun/Platform.sh build environment, do nothing.
+   if [ -z "${PLATFORM_CACHE_DIR}" ]; then
+       echo "Not running in an Upsun/Platform.sh build environment. Aborting $TOOL_NAME installation.";
+       exit 0;
+   fi
+}
+
+get_latest_version() {
+  # Get Latest version from GITHUB_ORG/$TOOL repo
+  TOOL_VERSION=$(curl --silent -H 'Accept: application/vnd.github.v3.raw' \
+    -L https://api.github.com/repos/$GITHUB_ORG/$TOOL_NAME/releases | jq -r '.[0].tag_name');
+}
+
+run $1
