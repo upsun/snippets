7b5c7c99c01588ab6f87dd4b8a2a1bcfff64c998
Florent Huck <flovntp@gmail.com>
2025-03-20 09:41:56 +0100

    new version of the decouple frontend source

--- src/decouple-frontend/components/utils.js
+++
@@ -1,7 +0,0 @@
-export function getBackendUrl() {
-    if ('API_HOST' in process.env) {
-        return process.env.API_SCHEME + "://" + process.env.API_HOST;
-    } else {
-        return "http://localhost:8000";
-    }
-}
