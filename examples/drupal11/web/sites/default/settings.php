d12754c30f5145f52e9277217b5a6b933c9f3f2a
Paul Gilzow <paul@gilzow.com>
2025-07-11 08:02:49 -0500

    Update settings.php

--- examples/drupal11/web/sites/default/settings.php
+++ examples/drupal11/web/sites/default/settings.php
@@ -1,3 +1,4 @@
+<?php
 // Automatically generated include for settings managed by ddev.
 if (getenv('IS_DDEV_PROJECT') == 'true' && file_exists(__DIR__ . '/settings.ddev.php')) {
   include __DIR__ . '/settings.ddev.php';
